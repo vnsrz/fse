e57fac32a68aeb6b06b61b7d49b768699374dbd9
viniciusroriz <fowardshift@gmail.com>
2022-12-18 12:07:24 -0300

    remove comments

--- trabalho_1/central/central.py
+++ trabalho_1/central/central.py
@@ -3,9 +3,6 @@ from threads import *
 def main():
     host = sys.argv[1]
     port = int(sys.argv[2])
-
-    # host = '192.168.1.103'                  
-    # port = 34315
     server = ConsoleThread(host, port)
     server.start()
     
