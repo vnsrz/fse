3d5bb2dbe79dae0ef369047e0291be454e6974b3
viniciusroriz <fowardshift@gmail.com>
2023-01-24 01:23:59 -0300

    add readme

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
-# Trabalho 1 - Automação predial
+# Trabalho 2 - Forno
 
-## 1. Instalação
+## 1. Instalação e Utilização
 
-1. Coloque os conteúdos da pasta `central/` na placa que servirá como servidor central.  
-2. Coloque os conteúdos da pasta `distrib/` nas placas que servirão como servidores distribuídos.
-3. Nos servidores distribuidos, execute `pip install -r requirements.txt` para instalar as dependências.
-4. No servidor central, execute `python3 central.py <host> <port>` substituindo `<host>` e `<port>` pelo ip e porta da placa, respectivamente.
-5. Nos servidores distribuídos, execute `python3 control.py <config.json>` substituindo `<config.json>` pelo seu arquivo com a configuração da placa (alguns foram disponibilizados como exemplo).
+1. Execute `pip install -r requirements.txt` para instalar as dependências.
+2. Execute `python3 main.py` para rodar o programa
+3. O menu possui 2 opções, alterar os valores do pid (kp, ki, e kd), e informar uma temperatura de referência para ser usada no modo debug. As interações principais devem ser feitas pelo dashboard.
 
-## 2. Telas
-### 2.1 Console principal
-<img src="assets/console_principal.png" alt="" width="600" /><br>
+## 2. Experimento - Temperatura fixa
+Esse experimento iniciou com a placa na temperatura de 69.44 ºC e acompanhou sua descida até 48.94 ºC, onde subiu até chegar em 60.81 ºC.
 
-### 2.2 Console da sala
-<img src="assets/console_sala.png" alt="" width="300" /><br>
+### 2.1 Temperaturas
+<img src="assets/TemperaturaxTempo.png" alt="" width="600" /><br>
 
-### 2.3 Console de lâmpadas
-<img src="assets/console_lampadas.png" alt="" width="300" /><br>
-
+### 2.2 Acionamento da ventoinha/resistor
+<img src="assets/AcionamentoxTempo.png" alt="" width="600" /><br>
--- trabalho_2/main.py
+++ trabalho_2/main.py
@@ -22,42 +22,46 @@ Aperte enter para atualizar as temperaturas
 
 event = Event()
 
-def write_log(t_int, t_ext, t_ref, sinal_v, sinal_r):
+def write_log(t_int, t_ref, t_ext, sinal_v, sinal_r):
             if not os.path.isfile('./log.csv'):
                 with open('log.csv', 'a', encoding='UTF8') as f:
                     f.write('data,temp_interna,temp_externa,temp_referencia,acionamento_ventoinha,acionamento_resistor\n') 
             with open('log.csv', 'a', encoding='UTF8') as f:
-                f.write(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")},{t_int:.2f},{t_ext:.2f},{t_ref:.2f},{sinal_v},{sinal_r}\n')
+                f.write(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")},{t_int:.2f},{t_ref:.2f},{t_ext:.2f},{sinal_v:.2f},{sinal_r:.2f}\n')
 
 
 def cls() -> None:
-        os.system('clear')
+    for _ in range(50):
+        print()
 
 
 def menu(ctrl:Controle, temp:Temperatura):
     while True:
         if event.is_set():
-            print('menu fechando')
+            #print('menu fechando')
             break
-
         print(f'temp ref:\t {ctrl.temp_referencia:.2f} °C\ntemp int:\t {ctrl.temp_interna:.2f} °C\ntemp ext:\t {ctrl.temp_externa:.2f} °C')
         print(MENU)
         escolha = input()
+        cls()
 
         if escolha == '1':
-            kp = input('digite o valor de kp: ')
-            ki = input('digite o valor de ki: ')
-            kd = input('digite o valor de kd: ')
+            kp = float(input('digite o valor de kp:\n'))
+            ki = float(input('digite o valor de ki:\n'))
+            kd = float(input('digite o valor de kd:\n'))
             temp.altera_param(kp, ki, kd)
         elif escolha == '2':
-            ctrl.temp_usuario = float(input('digite a temperatura desejada: '))
-            ctrl.cmd.envia_sinal_referencia(ctrl.temp_usuario)
+            ctrl.temp_usuario = float(input('digite a temperatura desejada:\n'))
+            if ctrl.modo_controle:
+                ctrl.temp_referencia = ctrl.temp_usuario
+                ctrl.cmd.envia_sinal_referencia(ctrl.temp_referencia)
         
 
 def main():
     def signal_handler(sig, frame):
         print('\nSIGINT recebido')
         event.set()
+        print('aperte enter para encerrar o menu')
         temp.join()
         ctrl.join()
         sys.exit(0)
